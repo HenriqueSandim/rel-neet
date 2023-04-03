0679ac600cc7c9affd7b9aedaddfb23f3c330d9f
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-04-02 22:40:36 -0300

    chore: documentation to project done

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
+import { GlobalCSS } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalCSS />
+      <Component {...pageProps} />
+    </>
+  )
 }
